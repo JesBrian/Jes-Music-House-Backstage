1df687367cd4dace1ab03aa897f1059128ac03aa
JesBrian <JesBrian666@gmail.com>
2017-10-15 19:58:57 +0800

    Index修改Js代码[提高效率]

--- index/js/index/common.js
+++ index/js/index/common.js
@@ -1,21 +1,11 @@
 
 
-$("#leftNavbar>a").click(function () {
+$("#leftNavbar").find($("a")).click(function () {
     iframeLocation($(this).attr("from"), $(this).attr("to"));
-    $("#leftNavbar>a").removeClass("active");
+    $("#leftNavbar").find("a").removeClass("active");
     $(this).addClass("active");
 });
 
 function iframeLocation(ifName, url) {
     $("#" + ifName).attr("src",url);
 }
-
-
-/**
- * 鼠标移动到某个音乐列表上显示操作
- */
-$("table.specialTable tr").mousemove(function () {
-    $(this).find($("span.songlistOper")).fadeIn(38);
-}).mouseleave(function () {
-    $(this).find($("span.songlistOper")).fadeOut(38);
-});
