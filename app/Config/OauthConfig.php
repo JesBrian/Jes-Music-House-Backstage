4c4ef5d92202fa8ab16e4c3b13182b11cc1e2e42
JesBrian <jesbrian666@gmail.com>
2018-09-09 16:45:36 +0800

    lyric - [ add timestamp property ]

--- app/Config/OauthConfig.php
+++ app/Config/OauthConfig.php
@@ -5,28 +5,34 @@ namespace App\Config;
 class OauthConfig
 {
     public const OAUTH_CONFIG = [
-        'baiduOAuth2AK' => '百度AK',
-        'baiduOAuth2SK' => '百度SK',
-        'baiduCallbackUrl' => '百度回调函数',
+        // baidu
+        'baiduOAuth2AK' => 'f7weT5KIlTd8N85b9gTOkjR3',
+        'baiduOAuth2SK' => 'Hcm3kQ6csWlQ7fDXS6gUN0PdqArIw5HU',
+        'baiduCallbackUrl' => 'http://local.musichouse.cn/api/baiduOAuthCallBack',
 
-        'codingOAuth2CI' => 'condingCI',
-        'codingOAuth2CS' => 'codingCS',
-        'codingCallbackUrl' => 'coding回调函数',
+        // coding
+        'codingOAuth2CI' => 'c240a9f92b26e428025b25e075969c92e1d1353d',
+        'codingOAuth2CS' => 'c240a9f92b26e428025b25e075969c92e1d1353d',
+        'codingCallbackUrl' => 'http://local.musichouse.cn/api/codingOAuthCallBack',
 
-        'giteeOAuth2CI' => '码云CI',
-        'giteeOAuth2CS' => '码云CS',
-        'giteeCallbackUrl' => '码云回调函数',
+        // gitee
+        'giteeOAuth2CI' => '9d44e8042dc0b48b2b7b78810ffe01dd3941e68a3349b9eb9f859777e938ad87',
+        'giteeOAuth2CS' => '44d1b662e4e2efe1162184b915063adc458b1673b8a265356016857fa04ffb97',
+        'giteeCallbackUrl' => 'http://local.musichouse.cn/api/giteeOAuthCallBack',
 
-        'githubOAuth2CI' => 'githubCI',
-        'githubOAuth2CS' => 'githubCS',
-        'githubCallbackUrl' => 'github回调函数',
+        // github
+        'githubOAuth2CI' => '87c8d2b97928d405f78f',
+        'githubOAuth2CS' => '36ba4a44bb234b19b3296499ad5d36152f4a70af',
+        'githubCallbackUrl' => 'http://local.musichouse.cn/api/githubOAuthCallBack',
 
-        'qqOAuth2AI' => '腾讯AI',
-        'qqOAuth2AK' => '腾讯AK',
-        'qqCallbackUrl' => '腾讯回调函数',
+        // qq
+        'qqOAuth2AI' => '101481602',
+        'qqOAuth2AK' => 'ebad3afbcabb4789c9a3d88d678756cf',
+        'qqCallbackUrl' => 'http://local.musichouse.cn/api/qqOAuthCallBack',
 
-        'weiboOAuth2AK' => '微博AK',
-        'weiboOAuth2AS' => '微博AS',
-        'weiboCallbackUrl' => '微博回调函数',
+        // weibo
+        'weiboOAuth2AK' => '1232002919',
+        'weiboOAuth2AS' => '97832caec4bb0202cd9c6d60f5d77b00',
+        'weiboCallbackUrl' => 'http://local.musichouse.cn/api/weiboOAuthCallBack',
     ];
 }
